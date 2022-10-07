054a1ab91b9831bcd0461e7be50d77a7d4211678
robogf <111620315+robogf@users.noreply.github.com>
2022-10-06 21:31:46 -0700

    added our timing to the schedule

--- index.js
+++ index.js
@@ -5,4 +5,21 @@
 
     $("#currentDay").text(moment().format("dddd,MMMM Do YYYY"));
 
-    $(
+    var saveBtn = $(".saveBtn")
+
+    function calendarBackground () {
+        var currentHour = moment().format('H')
+        $(".time-block").each(function(){
+            var hour = parseInt($(this).attr("id"));
+            if (hour>currentHour){
+                $(this).addClass("future");
+            }
+                else if(hour===currentHour){
+                    $(this).addClass("present")
+                } 
+                else{
+                    $(this).addClass("past")
+                }
+                    })
+        }
+calendarBackground();
